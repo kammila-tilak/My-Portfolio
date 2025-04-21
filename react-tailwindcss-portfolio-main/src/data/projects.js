a9299cef4c4abc11ea93c1e7e118a48caa0232d7
kammila-tilak <kammilatilak@gmail.com>
2025-04-22 00:27:38 +0530

    initial commit

---
+++ react-tailwindcss-portfolio-main/src/data/projects.js
@@ -0,0 +1,25 @@
+// Import images
+import WebImage2 from '../images/rock-paper-scissors.jpg';
+import MobileImage2 from '../images/preview.jpg';
+import UIImage1 from '../images/temperature-converter.jpg';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Rock Paper Scissors',
+		category: 'Web Application',
+		img: WebImage2,
+	},
+	{
+		id: 2,
+		title: 'Scientific Calculator',
+		category: 'Web Application',
+		img: MobileImage2,
+	},
+	{
+		id: 3,
+		title: 'Temperature Converter',
+		category: 'Web Application',
+		img: UIImage1,
+	},
+];
